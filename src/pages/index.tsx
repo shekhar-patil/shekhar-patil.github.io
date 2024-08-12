c5e13a365429d9afe392e22cb459a8ae4aa5a01e
Shekhar Patil <patilshekhar900@gmail.com>
2024-08-12 11:16:59 +0530

    Add website description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Blogs, Tutorials, Guidance | ${siteConfig.title}`}
+      description="Blogs, Tutorials, Guidance by Shekhar Patil">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
